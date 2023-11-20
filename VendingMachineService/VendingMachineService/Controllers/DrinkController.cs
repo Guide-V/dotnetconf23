7c8bd2803a431060bc5b3b85a4aff67b57cdf7f7
guidev <vnicklamai@gmail.com>
2023-11-20 20:22:54 +0700

    Add initial projects and boilerplates

---
+++ VendingMachineService/VendingMachineService/Controllers/DrinkController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendingMachineService.Models;
+
+namespace VendingMachineService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DrinkController : Controller
+    {
+        private readonly ILogger<DrinkController> _logger;
+        public DrinkController(ILogger<DrinkController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "OrderDrink")]
+        public DrinkOrder Post(DrinkOrder drinkOrder)
+        {
+            return new DrinkOrder
+            {
+                Id = drinkOrder.Id,
+                Price = drinkOrder.Price,
+                CreatedDate = DateTime.Now,
+                HasStraw = drinkOrder.HasStraw
+            };
+        }
+    }
+}
---
+++ VendingMachineService/VendingMachineService/Models/DrinkOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace VendingMachineService.Models
+{
+    public class DrinkOrder
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool HasStraw { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set;}
+    }
+}
